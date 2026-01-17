aa1521c99f57841023d571f313b52d3bbe82eeca
Ambuj Kumar Tripathi <tripathiambuj150@gmail.com>
2026-01-18 00:02:45 +0530

    fix: Update text splitter import and add dependency

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ import os
 import re
 from dotenv import load_dotenv
 from langchain_community.document_loaders import PyMuPDFLoader, DirectoryLoader
-from langchain.text_splitter import RecursiveCharacterTextSplitter
+from langchain_text_splitters import RecursiveCharacterTextSplitter
 from langchain_huggingface import HuggingFaceEmbeddings
 from langchain_chroma import Chroma
 from langchain_openai import ChatOpenAI
